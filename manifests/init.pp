9ebcb36b1437c0bb890e0406acb6203130319d61
Calvin Hartwell <mail@calvinhartwell.com>
2015-11-17 09:19:25 +0400

    Fixed module class and improved motd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 # motd
 
-class motd { 
+class satellite-motd { 
 	# motd.erb file must exist in motd/template folder
-	$motd_content = template('motd/motd.erb')
+	$motd_content = template('satellite-motd/motd.erb')
 	
 	# ensure motd file is placed in etc 
 	file { '/etc/motd':
